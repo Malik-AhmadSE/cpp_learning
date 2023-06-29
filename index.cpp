01f47541379e2c3edbcec219de56e9719e27bfa1
ahmad <ahmadali009ali008@gmail.com>
2023-06-29 22:42:20 +0500

    using  int in my function

--- index.cpp
+++ index.cpp
@@ -3,4 +3,48 @@
 // 2. object oriented programing (OOP)
 
 
+//// functional data 
+
+/// 1.varaibles 
+/// 2.datatypes
+/// 3.loops
+///4.conditons 
+// 5.switch
+// 6.functions 
+// 7.opeartor overloading
+// 8.file handling 
+
+///// library and modules 
+
+/// 1. let say quaid-e-azam library 
+/// 2. every book in the library is a module
+
+//// library ->  iostream ,windows.h,stdio.h,string,conio.h,
+
+////  module ->  iostream -> modules-> input -> cin , output -> cout
+
+
+////// variables -> use to store date, use to store different data.
+/// c=10; -> use to store data , 
+/// cin>> c; ->use to store user data or different data.
+
+
+
+/// datatype 
+
+/// datatype -> we tell computer or restrict to store which type of data.
+
+///  int,float, string,char,double,bool
+
+
+#include<iostream>
+using namespace std;
+int main(){
+	int a; // where int is datatype and a is a variable name . NOTE varaible name can be anything.
+	cin>>a;
+	cout<<a;
+}
+
+
+
 
